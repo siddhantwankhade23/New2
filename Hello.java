760f339f7f2fca8fedb1588613d87ed98e657d56
Siddhant Wankhade <siddhantwankhade23@gmail.com>
2024-06-17 21:46:22 +0530

    msg change

--- Hello.java
+++ Hello.java
@@ -5,5 +5,8 @@ class Test
         System.out.println("NewMaster Branch");
 
         System.out.println("NewMaster Branch new statement");
+
+        System.out.println("master changes");
+        
     }
 }
